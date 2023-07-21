fd66960f4fe1e44b0d7623ea7cc923f4152914e4
Binney <sar.binney@hotmail.co.uk>
2023-07-21 23:22:20 +0100

    print realistic things

--- paint/cp.py
+++ paint/cp.py
@@ -1,23 +1,46 @@
-# Write your code here :-)
-print("Hello world!")
-
 from adafruit_circuitplayground import cp
 import time
 import board
 import busio
 
-counter = 3
-colour = 0
+a = 0
+b = -1
+colour = (10,10,10)
+
+def select_tool(num):
+    if num == 0:
+        print("pencil")
+    elif num == 1:
+        print("fill")
+    elif num == 2:
+        print("erase")
+    else:
+        print("line")
+
+def select_colour(num):
+    if num == 0:
+        print("red")
+        return (30, 0, 0)
+    if num == 1:
+        print("yellow")
+        return (20, 10, 0)
+    if num == 2:
+        print("green")
+        return (0, 30, 0)
+    else:
+        print("blue")
+        return (0, 0, 30)
 
 while True:
     if cp.button_a:
-        counter += 1
-        print(counter)
+        a = (a + 1) % 4
+        select_tool(a)
 
     if cp.button_b:
-        colour = (colour + 1) % 30
-        print("Colour: " + str(colour))
+        b = (b + 1) % 4
+        colour = select_colour(b)
+
     cp.pixels.fill((0,0,0))
-    cp.pixels[counter % 10] = (30 - colour, colour, 0)
-    time.sleep(0.1)
+    cp.pixels[a] = colour
+    time.sleep(0.2)
 
