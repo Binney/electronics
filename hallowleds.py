40a834f2c52b809d6a1fd67e814f59b635ea5155
Catalin Ursachi <catalin.ursachi@softwire.com>
2023-10-21 20:21:25 +0100

    updated leds

--- hallowleds.py
+++ hallowleds.py
@@ -2,37 +2,43 @@ from gpiozero import LED
 from time import sleep
 import random
 
-G = LED(0)
-H = LED(1)
-O = LED(2)
-S = LED(3)
-T = LED(4)
-I = LED(5)
-D = LED(6)
-C = LED(7)
-L = LED(8)
-U = LED(9)
-E = LED(10)
+G = LED(4)
+H = LED(17)
+O = LED(27)
+S = LED(22)
+T = LED(5)
+I = LED(6)
+D = LED(13)
+C = LED(19)
+L = LED(26)
+U = LED(20)
+E = LED(21)
 
+all_leds = [G, H, O, S, T, I, D, C, L, U, E]
 answer = [G, O, O, U, T, S, I, D, E, D, I, G]
 
 while True:
     for led in answer:
         led.on()
-        sleep(2)
+        sleep(0.2)
         led.off()
-        sleep(1)
+        sleep(0.1)
 
     # all blink complete sequence
     for i in range(5):
-        for led in answer:
+        for led in all_leds:
             led.on()
-        sleep(1 / (i ** 2))
-        for led in answer:
+        sleep(1 / ((i + 1) ** 2))
+        for led in all_leds:
             led.off()
 
     sleep(5)
 
-    shuffled_answer = answer.copy()
-    shuffled_answer.append(answer)
-    random.shuffle(shuffled_answer)
+    shuffled_leds = all_leds.copy()
+    shuffled_leds.extend(all_leds)
+    shuffled_leds.extend(all_leds)
+    shuffled_leds.extend(all_leds)
+    random.shuffle(shuffled_leds)
+    for led in shuffled_leds:
+        led.toggle()
+        sleep(0.2)
