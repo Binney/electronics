4e6c1865080e0b779583da2ca7182b7318682da0
Catalin Ursachi <catalin.ursachi@softwire.com>
2024-02-25 11:27:28 +0000

    zoooooom

---
+++ motor.py
@@ -0,0 +1,14 @@
+from gpiozero import Button, LED
+from time import sleep
+import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+
+print("Hello, world!")
+
+GpioPins = [14, 15, 18, 23]
+mymotortest = RpiMotorLib.BYJMotor("SyzyMotor", "28BYJ")
+mymotortest.motor_run(GpioPins, 0.001, 512, False, False, "half", 0.05)
+
+print("And that's done")
+
+
