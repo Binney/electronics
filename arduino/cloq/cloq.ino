051f6469d3aa6159ef8de3e74aff4332d9b1c89e
Binney <sar.binney@hotmail.co.uk>
2024-06-25 21:06:02 +0100

    nice calming animation

--- arduino/cloq/cloq.ino
+++ arduino/cloq/cloq.ino
@@ -67,14 +67,28 @@ void loop() {
     if (i % 2 == 0) {
       // Count inwards
       for (int j=end; j>=start; j--) {
-        strip.setPixelColor(j, strip.ColorHSV(i * 65536L / 12));
+        for (int k=end; k>=j; k--) {
+          strip.setPixelColor(k, strip.ColorHSV((i * 65536L / 12) + (k - j) * 65536L / 36));
+        }
+        strip.show();
+        delay(100);
+      }
+      for (int j=end; j>=start; j--) {
+        strip.setPixelColor(j, strip.Color(0, 0, 0));
         strip.show();
         delay(100);
       }
     } else {
       // Count outwards
       for (int j=start; j<=end; j++) {
-        strip.setPixelColor(j, strip.ColorHSV(i * 65536L / 12));
+        for (int k=start; k<=j; k++) {
+          strip.setPixelColor(k, strip.ColorHSV((i * 65536L / 12) + (j - k) * 65536L / 36));
+        }
+        strip.show();
+        delay(100);
+      }
+      for (int j=start; j<=end; j++) {
+        strip.setPixelColor(j, strip.Color(0, 0, 0));
         strip.show();
         delay(100);
       }
