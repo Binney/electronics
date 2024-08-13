652c7f6bf5e48ec89e05178c5fab15f8ffe2ede2
Binney <sar.binney@hotmail.co.uk>
2024-08-13 22:50:32 +0100

    take me ooooon

--- dotstar.py
+++ dotstar.py
@@ -16,16 +16,16 @@ print("letsgooo")
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 2, brightness=0.1, auto_write=False)
 
 import adafruit_dotstar as dotstar
-num_pixels = 100
-dots = dotstar.DotStar(board.SCK, board.MOSI, num_pixels, brightness=0.2)
+num_pixels = 118 # I don't even know
+dots = dotstar.DotStar(board.SCK, board.MOSI, num_pixels, brightness=0.2, auto_write=False)
 
 RED = (255, 0, 0)
+ORANGE = (255, 50, 0)
 YELLOW = (255, 150, 0)
 GREEN = (0, 255, 0)
 CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
-dots.fill(CYAN)
 
 def rainbow_cycle(wait):
     for j in range(255):
@@ -34,15 +34,16 @@ def rainbow_cycle(wait):
         for i in range(num_pixels):
             rc_index = (i * 256 // num_pixels) + j
             dots[i] = colorwheel(rc_index & 255)
-        #dots.show()
+        dots.show()
         time.sleep(wait)
 
 def show_colour(color):
     pixels.fill(color)
     pixels.show()
+    dots.fill(color)
+    dots.show()
 
-while True:
-    rainbow_cycle(0)  # Increase the number to slow down the rainbow
+rainbow_cycle(0)  # Increase the number to slow down the rainbow
 
 try:
     from audioio import AudioOut
@@ -59,16 +60,19 @@ button = digitalio.DigitalInOut(board.A1)
 button.switch_to_input(pull=digitalio.Pull.UP)
 
 show_colour(BLUE)
+
+from audiomp3 import MP3Decoder
+
 print("looking for chicken")
-wave_file = open("meow1.wav", "rb")
-wave = WaveFile(wave_file)
+mp3_file = open("take_on_me_short.mp3", "rb")
+file_to_play = MP3Decoder(mp3_file)
 audio = AudioOut(board.A0)
 
 
 while True:
     print("let's go")
     show_colour(CYAN)
-    audio.play(wave)
+    audio.play(file_to_play)
     print("i play it")
 
     show_colour(YELLOW)
@@ -77,8 +81,8 @@ while True:
     while time.monotonic() - t < 6:
         pass
 
-    show_colour(GREEN)
-    audio.pause()
+    show_colour(ORANGE)
+    #   audio.pause()
     print("Waiting for button press to continue!")
     while button.value:
         pass
