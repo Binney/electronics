8baa38e0505aaece2cb90a7300c43564453342f7
Sarah Binney <sarah.binney@softwire.com>
2024-08-18 12:18:50 +0100

    fone

---
+++ answerphone/answerphone.py
@@ -0,0 +1,37 @@
+from pyaudio import PyAudio, paInt16
+import wave
+
+print("Hello world!")
+p = PyAudio()
+
+for ii in range(p.get_device_count()):
+    print(p.get_device_info_by_index(ii))
+
+chunk = 1024
+sample_format = paInt16
+channels = 2
+fs = 44100
+seconds = 5
+filename = "output.wav"
+
+print("Recording")
+
+stream = p.open(format=sample_format, channels=channels, rate=fs, frames_per_buffer=chunk, input=True)
+
+frames = []
+
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
+
+wf = wave.open(filename, "wb")
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
