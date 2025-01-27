a1cbfe8803f648980095f407a4882d339f063be1
Sarah Binney <sarah.binney@softwire.com>
2025-01-27 18:55:07 +0000

    more 7seg

--- 7_seg.py
+++ 7_seg.py
@@ -6,22 +6,13 @@ from adafruit_ht16k33.segments import Seg7x4
 i2c = busio.I2C(board.GP27, board.GP26)
 display = Seg7x4(i2c)
 display.print("9999")
-sleep(5)
+sleep(1)
+display.print("    ")
+sleep(0.5)
 
 while True:
-    display.print("HELL")
-    sleep(0.5)
-    display.print("ELL0")
-    sleep(0.5)
-    display.print("LL0 ")
-    sleep(0.5)
-    display.print("L0  ")
-    sleep(0.5)
-    display.print("0   ")
-    sleep(0.5)
-    display.print("   H")
-    sleep(0.5)
-    display.print("  HE")
-    sleep(0.5)
-    display.print(" HEL")
-    sleep(0.5)
+    message = "HELL0 ALL   "
+    doublemsg = message + message
+    for i in range(len(message)):
+        display.print(doublemsg[i:4+i])
+        sleep(0.5)
