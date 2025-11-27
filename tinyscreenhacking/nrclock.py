37c1f82b3fd6909bc9d10b055582210bcaf278d9
Sarah Binney <Sarah.Binney@softwire.com>
2025-11-27 19:52:42 +0000

    some size tweaks and cleanup

--- tinyscreenhacking/nrclock.py
+++ tinyscreenhacking/nrclock.py
@@ -50,30 +50,6 @@ print("✅ Wifi!")
 
 ntp = adafruit_ntp.NTP(pool, tz_offset=0)  # tz_offset in hours (0 for UTC, or -5 for EST, etc.)
 
-# print(f" | ✅ JSON GET Test: {weather_url}")
-# with requests.get(weather_url) as response:
-#     json_resp = response.json()
-#     print(json_resp)
-#     # Parse out the 'data' key from json_resp dict.
-#     print(f" | ✅ JSON 'value' Response: {json_resp['timezone']}")
-# print("-" * 80)
-
-# # Set text, font, and color
-# font = terminalio.FONT
-# color = 0x0000FF
-
-# time_temp = []
-# for i in range(len(json_resp["hourly"]["time"])):
-#     time = json_resp["hourly"]["time"][i]
-#     temp = json_resp["hourly"]["temperature_2m"][i]
-#     time_temp.append([time, temp])
-    
-
-UPDATE_INTERVAL_MS = int(os.getenv("UPDATE_INTERVAL_MS"))
-start_time = system_time.monotonic()
-
-current_index = 0 # keeps track of the index of the temperature we're showing
-
 def arrow(size, skew, thickness, x, y, angle):
     points = [(0, 0), (size, skew), (size + thickness, skew), (thickness, 0), (size + thickness, -skew), (size, -skew)]
     points = [
@@ -84,16 +60,17 @@ def arrow(size, skew, thickness, x, y, angle):
     points = [(p[0] + x, p[1] + y) for p in points]
     return FilledPolygon(points, fill=0xdb010e)
 
-r1 = 90
-r2 = 80
+r1 = 95
+r2 = 83
 
 # Main loop
 while True:
-    # The code below uses the drawing api to render to the screen, see https://learn.adafruit.com/circuitpython-display-support-using-displayio/ui-quickstart
-    my_display_group = displayio.Group() # https://learn.adafruit.com/circuitpython-display-support-using-displayio/ui-quickstart#groups-3033357
+    now = system_time.monotonic()
 
-    inner_clock_outline = Circle(display.width // 2, display.height // 2, r1, outline=0xdb010e, stroke=4)
-    outer_clock_outline = Circle(display.width // 2, display.height // 2, r2, outline=0xdb010e, stroke=4)
+    my_display_group = displayio.Group()
+
+    inner_clock_outline = Circle(display.width // 2, display.height // 2, r1 + 2, outline=0xdb010e, stroke=5)
+    outer_clock_outline = Circle(display.width // 2, display.height // 2, r2 + 3, outline=0xdb010e, stroke=5)
     my_display_group.append(inner_clock_outline)
     my_display_group.append(outer_clock_outline)
 
@@ -104,35 +81,36 @@ while True:
     time_label.anchored_position = (display.width // 2, display.height // 2)
     my_display_group.append(time_label)
 
-    seconds = time_value.tm_sec
-    angle = radians(seconds * 6)
+    # seconds = time_value.tm_sec
+    # we are actually faking this entirely
+    seconds = (now % 60) + (now % 1)
+    angle = radians(seconds * 6 + 180)
     sec_hand_coords = (display.width // 2 + int(r1 * -1 * sin(angle)),
                        display.height // 2 + int(r1 * cos(angle)))
-    tr1 = arrow(10, 8, 6, sec_hand_coords[0], sec_hand_coords[1], angle)
+    tr1 = arrow(15, 11, 7, sec_hand_coords[0], sec_hand_coords[1], angle)
     my_display_group.append(tr1)
 
     sec_hand_coords = (display.width // 2 + int(r2 * -1 * sin(angle)),
                        display.height // 2 + int(r2 * cos(angle)))
-    tr2 = arrow(-10, 8, -6, display.width - sec_hand_coords[0], sec_hand_coords[1], -angle)
+    tr2 = arrow(-15, 11, -7, display.width - sec_hand_coords[0], sec_hand_coords[1], -angle)
     my_display_group.append(tr2)
 
     # Here's an example of how to use the touch screen. Docs: https://docs.circuitpython.org/projects/cst8xx/en/latest/
     # The touchscreen is intialised at the start of the program
     # You only get the x, y coordinates of the touch, so you'll have to cross reference that with the coordinates you put your ui elements at.
-    touch_text = "no touch detected"
-    if ctp.touched:
-        touch = ctp.touches[0]
-        x = touch["x"]
-        y = touch["y"]
-        event = events[touch["event_id"]]
-        touch_text = f"touch at x: {x}, y: {y}, event: {event}"
+    # touch_text = "no touch detected"
+    # if ctp.touched:
+    #     touch = ctp.touches[0]
+    #     x = touch["x"]
+    #     y = touch["y"]
+    #     event = events[touch["event_id"]]
+    #     touch_text = f"touch at x: {x}, y: {y}, event: {event}"
 
-    touch_label = label.Label(terminalio.FONT, text=touch_text)
-    touch_label.x = 20
-    touch_label.y = 200
-    my_display_group.append(touch_label)
+    # touch_label = label.Label(terminalio.FONT, text=touch_text)
+    # touch_label.x = 20
+    # touch_label.y = 200
+    # my_display_group.append(touch_label)
 
-    # Show the group we've just built up.
     display.root_group = my_display_group
 
     
